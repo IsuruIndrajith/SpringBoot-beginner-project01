96e7797ac949685fbe3d59c4df6d4a128215d337
Isuru <isuruindrajith680@gmail.com>
2025-01-18 17:17:54 +0530

    Getting response from a Rest API-Testing

--- springboot-rest-api/src/main/java/beginner_project_1/springboot_rest_api/controller/HelloWorldController.java
+++ springboot-rest-api/src/main/java/beginner_project_1/springboot_rest_api/controller/HelloWorldController.java
@@ -1,7 +1,6 @@
 package beginner_project_1.springboot_rest_api.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //making this java class a spring MUC component
@@ -13,5 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 //@RestController annotation is the combination of these @Responsebody and @Controller annotations
 @RestController
+//when we are creating a restful web services using Spring MUC use @RestController
 public class HelloWorldController {
+
+//    HTTP GET request
+//    this below annotation is to handle HTTP get requests
+//    Client accessing URL: http://localhost:8080/hello-world
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World!";
+    }
 }
